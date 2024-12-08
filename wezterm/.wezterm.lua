8be09da1fe59ec3bd408fa8a2641d3c0b6aa1ea0
Zach Fuller <zach.fuller1222@gmail.com>
2024-12-07 18:49:24 -0700

    updated wezterm font and fixed typo in config name

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 -- config.font = wezterm.font 'Hack Nerd Font Mono'
--- config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
 -- config.font = wezterm.font("0xProto Nerd Font Mono")
 config.font_size = 13.0
 -- For example, changing the color scheme:
@@ -17,10 +17,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 config.enable_scroll_bar = true
 
--- start with WSL
--- config.default_domain = "WSL:Ubuntu-22.04"
-
-config.enabled_wayland = false
+config.enable_wayland = false
 
 -- and finally, return the configuration to wezterm
 return config
