c3d03854938a6f0387f3cf475bd2563c0286b323
Zach Fuller <zach.fuller1222@gmail.com>
2025-02-14 18:20:19 -0700

    feat(fish): add fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,10 @@
+starship init fish | source
+zoxide init fish | source
+
+# Set up fzf key bindings
+fzf --fish | source
+
+direnv hook fish | source
+pyenv init - fish | source
+
+uv generate-shell-completion fish | source
