e4f73075fb2f06fc839c8c03968ef6673e62bce8
Zach Fuller <zach.fuller1222@gmail.com>
2025-08-08 18:59:58 -0700

    chore(fish): lm studio path

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,3 +8,8 @@ direnv hook fish | source
 pyenv init - fish | source
 
 uv generate-shell-completion fish | source
+
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/zachfuller/.lmstudio/bin
+# End of LM Studio CLI section
+
