6b59cb7f727310e7bff5eda25783d336a3a3614b
Zach Fuller <zach.fuller1222@gmail.com>
2025-09-11 17:55:05 -0700

    chore(fish): update fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 starship init fish | source
 zoxide init fish | source
+atuin init fish | source
 
 # Set up fzf key bindings
 fzf --fish | source
@@ -13,3 +14,45 @@ uv generate-shell-completion fish | source
 set -gx PATH $PATH /Users/zachfuller/.lmstudio/bin
 # End of LM Studio CLI section
 
+# mise
+mise activate fish | source
+
+# AWS
+set -x AWS_DEFAULT_REGION "us-west-1"
+
+# eza aliases (parity with zsh)
+function ls
+	eza --color=always --group-directories-first --icons=always $argv
+end
+
+function ll
+	eza -la --icons=always --octal-permissions --group-directories-first $argv
+end
+
+function l
+	eza -bGF --header --git --color=always --group-directories-first --icons=always $argv
+end
+
+function llm
+	eza -lbGd --header --git --sort=modified --color=always --group-directories-first --icons=always $argv
+end
+
+function la
+	eza --long --all --group --group-directories-first $argv
+end
+
+function lx
+	eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --color=always --group-directories-first --icons=always $argv
+end
+
+function lS
+	eza -1 --color=always --group-directories-first --icons=always $argv
+end
+
+function lt
+	eza --tree --level=2 --color=always --group-directories-first --icons=always $argv
+end
+
+function l.
+	eza -a | grep -E '^\.'
+end
